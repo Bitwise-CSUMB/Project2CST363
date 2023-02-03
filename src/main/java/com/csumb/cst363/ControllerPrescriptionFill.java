cf583e9094bcf4ae049f0d94a37a94442b2e8698
Jake-CSUMB <116601871+Jake-CSUMB@users.noreply.github.com>
2023-02-03 13:04:14 -0800

    Reconcile app and database

--- src/main/java/com/csumb/cst363/ControllerPrescriptionFill.java
+++ src/main/java/com/csumb/cst363/ControllerPrescriptionFill.java
@@ -13,14 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller   
+@Controller
 public class ControllerPrescriptionFill {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 
-	/* 
+	/*
 	 * Patient requests form to search for prescription.
 	 */
 	@GetMapping("/prescription/fill")
@@ -36,19 +36,19 @@ public class ControllerPrescriptionFill {
 	 *     and uniquely identify a prescription and a pharmacy.
 	 * 2.  update prescription with pharmacyid, name and address.
 	 * 3.  update prescription with today's date.
-	 * 4.  Display updated prescription 
+	 * 4.  Display updated prescription
 	 * 5.  or if there is an error show the form with an error message.
 	 */
 	@PostMapping("/prescription/fill")
 	public String processFillForm(Prescription p,  Model model) {
 
 
-		// TODO  
+		// TODO
 
 		// temporary code to set fake data for now.
-		p.setPharmacyID("70012345");
+		p.setPharmacyId(70012345);
 		p.setCost(String.format("%.2f", 12.5));
-		p.setDateFilled( new java.util.Date().toString() );
+		p.setFillDate( new java.util.Date().toString() );
 
 		// display the updated prescription
 
