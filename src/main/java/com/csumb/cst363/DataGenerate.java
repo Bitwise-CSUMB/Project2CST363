6d053e993c0d66e3bccc7d0bcd434794d0df56e3
jeremiahmcgrath <jeremiah.revox@gmail.com>
2023-02-05 19:07:51 -0800

    Update DataGenerate.java

--- src/main/java/com/csumb/cst363/DataGenerate.java
+++ src/main/java/com/csumb/cst363/DataGenerate.java
@@ -7,11 +7,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Random;
 
+/**
+ * @author jeremiah
+ * This class clears the 'fill', 'prescription', 'patient'
+ * and 'doctor' rows, and generates random data for
+ * 100 patients, 10 doctors and 100 prescriptions
+ */
+
 public class DataGenerate {
 	
 	static final String DBURL = "jdbc:mysql://localhost:3306/cst363";  // database URL
 	static final String USERID = "root";
 	static final String PASSWORD = "<Poweruser123>";
+	
+	static final String[] specialties= {"Internal Medicine", "Family Medicine", "Pediatrics", "Orthpedics", "Dermatology",
+			"Cardiology", "Gynecology", "Gastroenterology", "Psychiatry", "Oncology"};
 	
 	public static void main(String args[]) {
 		
@@ -45,7 +55,32 @@ public class DataGenerate {
 			ps = conn.prepareStatement("delete from doctor");
 			row_count = ps.executeUpdate();
 			System.out.println("rows deleted "+row_count);
-						
+			
+			// generate doctor data and insert into table.  We want to generated column "id" value to be returned
+			// as a generated key
+			
+			String sqlINSERT = "insert into doctor(doctorSSN, doctorFirstName, doctorLastName, specialty, praticeSinceYear) values ( ?, ?, ?, ?, ?)";
+			String[] keycols = {"doctorId"};
+			ps = conn.prepareStatement(sqlINSERT, keycols);
+			
+			// insert 10 rows with data
+			for (int k=1; k<=10; k++) {
+				String practice_since = Integer.toString(2000+gen.nextInt(20));
+				// TODO ssn generated is not guaranteed to be unique.  This should be fixed.
+				String ssn = Integer.toString(123450000+gen.nextInt(10000));
+				ps.setString(1,  "Doctor Number"+k);
+				ps.setString(2, "Dr.");
+				ps.setString(3, specialties[k%specialties.length]);
+				ps.setString(4, practice_since);
+				ps.setString(5, ssn);
+				row_count = ps.executeUpdate();
+				System.out.println("row inserted "+row_count);
+				
+				rs = ps.getGeneratedKeys();
+				rs.next();
+				id = rs.getInt(1);
+				System.out.println("row inserted for doctor id "+id);
+			}
 		} catch (SQLException e) {
 			System.out.println("Error: SQLException "+e.getMessage());
 		}
